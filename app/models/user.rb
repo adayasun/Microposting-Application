c4d9160b54b62b19bdf67bb111b3992a54e82beb
Amy Dayasundara <adayasundara3085@conestogac.on.ca>
2020-04-12 15:36:50 +0000

    reverted

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-  
+  validates :password, presence: true, length: { minimum: 6 }
+
   # Returns the hash digest of the given string.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
